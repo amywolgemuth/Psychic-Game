79a8cc28e1f04f9dc86941a2bb85ee8615b6c7b3
Wolgemuth, Amy <amywolgemuth@gmail.com>
2019-05-11 08:55:15 -0500

    finished the game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,26 +4,25 @@ var losses = 0;
 var guessesLeft = 10;
 var guesses=[];
 var letterArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
 var compGuess = letterArr[Math.floor(Math.random() * letterArr.length)];
 
 document.onkeyup = function(event) {
     var userGuess = event.key.toLowerCase();
- 
-    
+
     console.log("computer guess: " + compGuess);
     console.log("user guess: " + userGuess);
 
     guesses.push(userGuess);
     document.getElementById("uGuess").innerHTML =guesses;
 
+
     if (userGuess === compGuess) {
         wins++;
         document.getElementById("uWin").innerHTML =wins;
         guessesLeft = 10;
         guesses = [];
-        alert("You Won!!!")
-
+        alert("You Won!!!");
+        compGuess = letterArr[Math.floor(Math.random() * letterArr.length)];
         }
     else {
         guessesLeft--;
@@ -34,6 +33,7 @@ document.onkeyup = function(event) {
             document.getElementById("uLoss").innerHTML =losses;
             guessesLeft = 10;
             guesses = [];
+            alert("Sorry, you lost!!");            compGuess = letterArr[Math.floor(Math.random() * letterArr.length)];
             }
-        }
+        }       
     }
